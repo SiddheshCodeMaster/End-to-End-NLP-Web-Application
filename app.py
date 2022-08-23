d62415217e59d4205338df43c7b3e858a293320c
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2022-08-23 23:08:30 +0530

    Flask Commit 1.1 - Final

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from flask import Flask, render_template, url_for
 from flask import request as req
-
+import requests as rs
 app = Flask(__name__)
-@app.route("/", methods=['GET','POST'])
-
+@app.route("/",methods=['GET','POST'])
 def Index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/Summarize", methods=['GET','POST'])
+@app.route("/Summarize",methods=["GET","POST"])
 def Summarize():
-    if req.methods == "POST":
+    if req.method == "POST":
         API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
-        headers = {"Authorization": f"Bearer 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"}
+        headers = {"Authorization": "Bearer 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"}
 
         data = req.form["data"]
 
+
         min_length = 20 #int(input("Enter the Minimum Length for the summary: "))
-        max_length = 70 #int(input("Enter the Maximum Length for the summary: "))
+        max_length = int(req.form["max_length"])
         def query(payload):
-            response = req.post(API_URL, headers=headers, json=payload)
+            response = rs.post(API_URL, headers=headers, json=payload)
             return response.json()
 
         output = query({
 	        "inputs": data,
 	        "parameters":{"min_length":min_length,"max_length":max_length},
         })[0]
-    
-        return render_template('index.html',result=output['summary_text'])
+
+        return render_template("index.html", result=output['summary_text'])
     else:
-        return render_template('index.html')
+        return render_template("index.html")
+    
 
 
 if __name__=="__main__":
-    app.debug=True # Keep it always True when your are debugging and making hcnages with your project
+    app.debug = True # While testing I am keeping debug = True.
     app.run()
+
--- dummy.py
+++ dummy.py
@@ -1,9 +1,10 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
-headers = {"Authorization": f"Bearer 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"}
+headers = {"Authorization": "Bearer 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"}
 
 data = '''The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.'''	
+
 
 min_length = int(input("Enter the Minimum Length for the summary: "))
 max_length = int(input("Enter the Maximum Length for the summary: "))
